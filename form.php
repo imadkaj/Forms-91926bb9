c57c305a243b6dd72d681731fac72ddee29b33ae
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-12-04 12:17:34 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>e-mail</title>
+
+    <meta charset="utf-8" />
+
+</head>
+
+<body>
+<h2>Contact Form</h2>
+<form class="form">
+    <p class="e-mail">
+        <input type="text" name="e-mail" id="e-mail" placeholder="mail@example.com" />
+        <label for="e-mail">e-mail</label>
+    </p>
+
+
+
+
+
+    <p class="submit">
+        <input type="submit" value="versturen" />
+    </p>
+</form>
+
+</body>
+
+</html>
